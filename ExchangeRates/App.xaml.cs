257800b2764f7b422e848cc37a4c6b91bcfb1dc0
AshRaven521 <ivan.kondrashov.92@mail.ru>
2024-12-10 12:50:02 +0300

    add dependency injection with mvvm structure

--- ExchangeRates/App.xaml.cs
+++ ExchangeRates/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +15,34 @@ namespace ExchangeRates
     /// </summary>
     public partial class App : Application
     {
+        public static IHost AppHost { get; private set; }
+
+        public App()
+        {
+            AppHost = Host.CreateDefaultBuilder()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddSingleton<MainWindow>();
+                }).Build();
+            
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            await AppHost.StartAsync();
+            var startFrom = AppHost.Services.GetRequiredService<MainWindow>();
+
+            startFrom.Show();
+
+            base.OnStartup(e);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            await AppHost.StopAsync();
+            
+
+            base.OnExit(e);
+        }
     }
 }
