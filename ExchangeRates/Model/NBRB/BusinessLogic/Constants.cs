5eabce6a4ed3ab68e64564a5656e54c21b6d99d8
AshRaven521 <ivan.kondrashov.92@mail.ru>
2024-12-20 11:45:22 +0300

    add constant references

---
+++ ExchangeRates/Model/NBRB/BusinessLogic/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace ExchangeRates.Model.NBRB.BusinessLogic
+{
+    public static class Constants
+    {
+        public static string NBRB_ALL_CURRENCIES_REFERENCE { get; } = "https://api.nbrb.by/exrates/currencies";
+        // Параметр periodicity = 0 означает, что получаем курс на сегодня
+        public static string NBRB_TODAY_RATES_REFERENCE { get; } = "https://api.nbrb.by/exrates/rates?periodicity=0";
+
+    }
+}
