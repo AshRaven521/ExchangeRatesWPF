42db6f0dd8f69a67edcfe2f21cf96d611dd06702
AshRaven521 <ivan.kondrashov.92@mail.ru>
2024-12-20 11:43:40 +0300

    add service for working with message box and file dialogs

---
+++ ExchangeRates/ViewModel/Services/DialogService.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Windows;
+
+namespace ExchangeRates.ViewModel.Services
+{
+    public class DialogService : IDialogService
+    {
+        public string OpenFile()
+        {
+            var openFileDialog = new OpenFileDialog();
+
+            openFileDialog.Filter = "Json files (*.json)|*.json";
+            openFileDialog.Title = "Выберите файл с курсами валют";
+            openFileDialog.Multiselect = false;
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                return openFileDialog.FileName;
+            }
+            return string.Empty;
+        }
+
+        public string SaveFile()
+        {
+            var saveFileDialog = new SaveFileDialog();
+
+            saveFileDialog.Filter = "Json files (*.json)|*.json";
+            saveFileDialog.Title = "Выберите файл для сохранения даных";
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                return saveFileDialog.FileName;
+            }
+            return string.Empty;
+        }
+
+        public void ShowErrorMessage(string errMessage)
+        {
+            MessageBox.Show(errMessage, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        public void ShowMessage(string message)
+        {
+            MessageBox.Show(message, "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        public void ShowWarningMessage(string warnMessage)
+        {
+            MessageBox.Show(warnMessage, "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+        }
+    }
+}
---
+++ ExchangeRates/ViewModel/Services/IDialogService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeRates.ViewModel.Services
+{
+    public interface IDialogService
+    {
+        void ShowMessage(string message);
+        void ShowErrorMessage(string errMessage);
+        void ShowWarningMessage(string warnMessage);
+        string OpenFile();
+        string SaveFile();
+    }
+}
