02ccf802aca3b49c00f5aa2885fea8f190225d6e
AshRaven521 <ivan.kondrashov.92@mail.ru>
2024-12-20 11:45:04 +0300

    add needed api models

---
+++ ExchangeRates/Model/NBRB/Entities/Rate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ExchangeRates.Model.NBRB.Entities
+{
+    public class Rate
+    {
+        [Key]
+        public int Cur_ID { get; set; }
+        public DateTime Date { get; set; }
+        public string Cur_Abbreviation { get; set; }
+        public int Cur_Scale { get; set; }
+        public string Cur_Name { get; set; }
+        public decimal? Cur_OfficialRate { get; set; }
+    }
+
+    public class RateShort
+    {
+        [Key]
+        public int Cur_ID { get; set; }
+        public DateTime Date { get; set; }
+        public decimal? Cur_OfficialRate { get; set; }
+    }
+}
